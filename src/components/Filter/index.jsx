b1b6373f14c21ec1d2167dfd5728296afebf20ab
ramatc <rtanquiascornejo@gmail.com>
2023-03-01 23:56:22 -0300

    Add media querys

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import Status from './category/Status';
 import Gender from './category/Gender';
 import Species from './category/Species';
 import './styles.css';
 
 const Filter = ({ filters, setFilters, setPageNumber }) => {
+
+    const [display, setDisplay] = useState(false);
 
     const handleClear = () => {
         setFilters({ status: '', gender: '', species: '' });
@@ -12,32 +15,40 @@ const Filter = ({ filters, setFilters, setPageNumber }) => {
 
     return (
         <>
-            <h2 className='filter-title'>Filters</h2>
-
-            <div className='filters'>
-                <Status
-                    setFilters={setFilters}
-                    setPageNumber={setPageNumber}
-                    {...filters}
-                />
-                <Gender
-                    setFilters={setFilters}
-                    setPageNumber={setPageNumber}
-                    {...filters}
-                />
-                <Species
-                    setFilters={setFilters}
-                    setPageNumber={setPageNumber}
-                    {...filters}
-                />
-            </div>
-
-            <button
-                onClick={handleClear}
-                className='btn-clear'
+            <h2
+                className='filter-title'
+                onClick={() => setDisplay(!display)}
             >
-                Clear filters
-            </button>
+                Filters
+                <img src='https://icongr.am/fontawesome/arrow-down.svg?size=20&color=ffffff' />
+            </h2>
+
+            <div>
+                <div className='filters'>
+                    <Status
+                        setFilters={setFilters}
+                        setPageNumber={setPageNumber}
+                        {...filters}
+                    />
+                    <Gender
+                        setFilters={setFilters}
+                        setPageNumber={setPageNumber}
+                        {...filters}
+                    />
+                    <Species
+                        setFilters={setFilters}
+                        setPageNumber={setPageNumber}
+                        {...filters}
+                    />
+                </div>
+
+                <button
+                    onClick={handleClear}
+                    className='btn-clear'
+                >
+                    Clear filters
+                </button>
+            </div>
         </>
     )
 }
